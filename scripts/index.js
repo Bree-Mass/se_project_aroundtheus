bb0e88995362b6c3804cec46afb937631e305915
Bree Massingill <breemassingill@gmail.com>
2024-05-03 15:40:50 -0500

    add index.js with first cards

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    name: "Biarritz",
+    link: "https://unsplash.com/photos/a-couple-of-large-rocks-sitting-on-top-of-a-beach-zCe5KtZXeCk",
+  },
+  {
+    name: "Mont Blanc massif",
+    link: "https://unsplash.com/photos/a-mountain-covered-in-snow-next-to-a-building-neox0-iCzYY",
+  },
+  {
+    name: "Tokyo",
+    link: "https://unsplash.com/photos/a-group-of-people-standing-outside-of-a-building-at-night-FEBYsUxEtW0",
+  },
+  {
+    name: "Madeira",
+    link: "https://unsplash.com/photos/the-sun-is-setting-over-the-ocean-near-a-rocky-shore-czP47CfKiyo",
+  },
+  {
+    name: "Westkapelle",
+    link: "https://unsplash.com/photos/a-light-house-sitting-in-the-middle-of-a-body-of-water-ICFp3tp_DBY",
+  },
+  {
+    name: "Київ",
+    link: "https://unsplash.com/photos/a-lush-green-field-with-trees-and-purple-flowers-plGJGSfmd3Q",
+  },
+];
+
+console.log(initialCards);
