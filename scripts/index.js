d634682dadaa972733c2618d8153a687e368cb8a
Bree Massingill <breemassingill@gmail.com>
2024-05-10 16:25:50 -0500

    add js for cards and remove unused image files

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,86 @@
-const initialCards = [
-  {
-    name: "Biarritz",
-    link: "https://unsplash.com/photos/a-couple-of-large-rocks-sitting-on-top-of-a-beach-zCe5KtZXeCk",
-  },
-  {
-    name: "Mont Blanc massif",
-    link: "https://unsplash.com/photos/a-mountain-covered-in-snow-next-to-a-building-neox0-iCzYY",
-  },
-  {
-    name: "Tokyo",
-    link: "https://unsplash.com/photos/a-group-of-people-standing-outside-of-a-building-at-night-FEBYsUxEtW0",
-  },
-  {
-    name: "Madeira",
-    link: "https://unsplash.com/photos/the-sun-is-setting-over-the-ocean-near-a-rocky-shore-czP47CfKiyo",
-  },
-  {
-    name: "Westkapelle",
-    link: "https://unsplash.com/photos/a-light-house-sitting-in-the-middle-of-a-body-of-water-ICFp3tp_DBY",
-  },
-  {
-    name: "Київ",
-    link: "https://unsplash.com/photos/a-lush-green-field-with-trees-and-purple-flowers-plGJGSfmd3Q",
-  },
-];
+////////// EDIT BUTTON //////////
+////////// EDIT BUTTON //////////
+////////// EDIT BUTTON //////////
 
-console.log(initialCards);
+let profileName = document.querySelector(".profile__name");
+let profileDesc = document.querySelector(".profile__description");
+const editButton = document.querySelector(".profile__edit-button");
 
-let modal = document.querySelector(".modal");
-let editButton = document.querySelector(".profile__edit-button");
-let closeButton = document.querySelector(".modal__close");
+const modal = document.querySelector(".modal");
+const closeButton = modal.querySelector(".modal__close-button");
+const form = modal.querySelector(".modal__form");
+let formName = modal.querySelector(".modal__name");
+let formDesc = modal.querySelector(".modal__description");
+
+function toggleModal() {
+  modal.classList.toggle("modal__opened");
+}
 
 editButton.addEventListener("click", function () {
-  modal.classList.add("modal__opened");
+  formName.value = profileName.textContent;
+  formDesc.value = profileDesc.textContent;
+  toggleModal();
 });
 
 closeButton.addEventListener("click", function () {
-  modal.classList.remove("modal__opened");
+  toggleModal();
+});
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  toggleModal();
+  profileName.textContent = formName.value;
+  profileDesc.textContent = formDesc.value;
+});
+
+////////// CARDS //////////
+////////// CARDS //////////
+////////// CARDS //////////
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+const cardTemplate =
+  document.querySelector("#card__template").content.firstElementChild;
+const cardGallery = document.querySelector(".gallery__cards");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardDesc = cardElement.querySelector(".card__description");
+
+  cardImage.src = data.link;
+  cardImage.setAttribute("alt", data.name);
+  cardDesc.textContent = data.name;
+
+  return cardElement;
+}
+
+initialCards.forEach((data) => {
+  const cardElement = getCardElement(data);
+  cardGallery.append(cardElement);
 });
