ee391a4db7baaacaeae34b74262193d765db1ff3
Bree Massingill <breemassingill@gmail.com>
2024-05-18 14:27:58 -0500

    fix refactor

--- scripts/index.js
+++ scripts/index.js
@@ -1,62 +1,4 @@
-////////// PROFILE EDIT BUTTON //////////
-////////// PROFILE EDIT BUTTON //////////
-////////// PROFILE EDIT BUTTON //////////
-
-const editProfileModal = document.querySelector("#edit-modal");
-const closeProfileButton = editProfileModal.querySelector(
-  ".modal__close-button"
-);
-const editButton = document.querySelector(".profile__edit-button");
-const profileForm = document.forms["profile-form"];
-const profileName = document.querySelector(".profile__name");
-const profileDesc = document.querySelector(".profile__description");
-const profileFormName = editProfileModal.querySelector("[name = 'name']");
-const profileFormDesc = editProfileModal.querySelector(
-  "[name = 'description']"
-);
-
-function toggleModal(modal) {
-  modal.classList.toggle("modal_opened");
-}
-
-editButton.addEventListener("click", () => {
-  profileFormName.value = profileName.textContent;
-  profileFormDesc.value = profileDesc.textContent;
-  toggleModal(editProfileModal);
-});
-
-closeProfileButton.addEventListener("click", () => {
-  toggleModal(editProfileModal);
-});
-
-profileForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  toggleModal(editProfileModal);
-  profileName.textContent = profileFormName.value;
-  profileDesc.textContent = profileFormDesc.value;
-});
-
-///////// ADD BUTTON //////////
-///////// ADD BUTTON //////////
-///////// ADD BUTTON //////////
-
-const addCardModal = document.querySelector("#add-modal");
-const closeAddButton = addCardModal.querySelector(".modal__close-button");
-const addCardButton = document.querySelector(".profile__add-button");
-const addForm = document.forms["add-card-form"];
-
-addCardButton.addEventListener("click", () => {
-  toggleModal(addCardModal);
-});
-
-closeAddButton.addEventListener("click", () => {
-  toggleModal(addCardModal);
-});
-
-////////// CARDS //////////
-////////// CARDS //////////
-////////// CARDS //////////
-
+// INITIAL CARDS
 const initialCards = [
   {
     name: "Yosemite Valley",
@@ -84,32 +26,90 @@ const initialCards = [
   },
 ];
 
+// MODAL
+const editProfileModal = document.querySelector("#edit-modal");
+const addCardModal = document.querySelector("#add-modal");
+const imageModal = document.querySelector("#image-modal");
+
+// FORMS
+const profileForm = document.forms["profile-form"];
+const profileName = document.querySelector(".profile__name");
+const profileDesc = document.querySelector(".profile__description");
+const profileFormName = editProfileModal.querySelector("[name = 'name']");
+const profileFormDesc = editProfileModal.querySelector(
+  "[name = 'description']"
+);
+const addCardForm = document.forms["add-card-form"];
+
+// CARDS
 const cardTemplate =
   document.querySelector("#card__template").content.firstElementChild;
 const cardGallery = document.querySelector(".gallery__cards");
+const imageModalImage = imageModal.querySelector(".modal__image");
 
+// BUTTONS
+const closeProfileButton = editProfileModal.querySelector(
+  ".modal__close-button"
+);
+const closeFormButton = addCardModal.querySelector(".modal__close-button");
+const editButton = document.querySelector(".profile__edit-button");
+const addCardButton = document.querySelector(".profile__add-button");
+const closeImageButton = imageModal.querySelector(".modal__close-button");
+
+// FUNCTIONS
+function toggleModal(modal) {
+  modal.classList.toggle("modal_opened");
+}
 function getCardElement(data) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardDesc = cardElement.querySelector(".card__description");
-
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const trashButton = cardElement.querySelector(".card__trash-button");
+  cardDesc.textContent = data.name;
   cardImage.src = data.link;
   cardImage.setAttribute("alt", data.name);
-  cardDesc.textContent = data.name;
-
+  cardImage.addEventListener("click", () => {
+    toggleModal(imageModal);
+  });
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+  trashButton.addEventListener("click", () => {
+    trashButton.parentElement.remove();
+  });
   return cardElement;
 }
-
 initialCards.forEach((data) => {
   const cardElement = getCardElement(data);
   cardGallery.append(cardElement);
 });
 
-////////// ADD CARD FORM //////////
-////////// ADD CARD FORM //////////
-////////// ADD CARD FORM //////////
-
-addForm.addEventListener("submit", (evt) => {
+// EVENT LISTENERS
+editButton.addEventListener("click", () => {
+  profileFormName.value = profileName.textContent;
+  profileFormDesc.value = profileDesc.textContent;
+  toggleModal(editProfileModal);
+});
+closeProfileButton.addEventListener("click", () => {
+  toggleModal(editProfileModal);
+});
+profileForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  toggleModal(editProfileModal);
+  profileName.textContent = profileFormName.value;
+  profileDesc.textContent = profileFormDesc.value;
+});
+addCardButton.addEventListener("click", () => {
+  toggleModal(addCardModal);
+});
+closeFormButton.addEventListener("click", () => {
+  toggleModal(addCardModal);
+});
+closeImageButton.addEventListener("click", () => {
+  toggleModal(imageModal);
+});
+addCardForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const addFormTitle = addCardModal.querySelector("[name = 'title']");
   const addFormImage = addCardModal.querySelector("[name = 'url']");
@@ -119,28 +119,4 @@ addForm.addEventListener("submit", (evt) => {
   toggleModal(addCardModal);
   addFormTitle.value = "";
   addFormImage.value = "";
-});
-
-////////// TRASH BUTTON //////////
-////////// TRASH BUTTON //////////
-////////// TRASH BUTTON //////////
-
-const trashButtons = document.querySelectorAll(".card__trash-button");
-
-trashButtons.forEach((trash) => {
-  trash.addEventListener("click", () => {
-    trash.parentElement.remove();
-  });
-});
-
-////////// LIKE BUTTON //////////
-////////// LIKE BUTTON //////////
-////////// LIKE BUTTON //////////
-
-const likeButtons = document.querySelectorAll(".card__like-button");
-
-likeButtons.forEach((heart) => {
-  heart.addEventListener("click", () => {
-    heart.classList.toggle("card__like-button_active");
-  });
 });
